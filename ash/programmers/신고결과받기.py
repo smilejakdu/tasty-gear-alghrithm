e3e5f07fc20c9808080fa37a7778c9f61645e713
smilejakdu <ash982416@gmail.com>
2022-04-28 19:30:00 +0900

    ash/로또

--- ash/programmers/신고결과받기.py
+++ ash/programmers/신고결과받기.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 '''
 id_list = ["muzi", "frodo", "apeach", "neo"]
 report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
@@ -5,15 +6,16 @@ k = 2
 result = [2,1,1,0]
 '''
 
-# user_list = ["muzi", "frodo", "apeach", "neo"]
-# report = ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"]
-# k = 2
+user_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"]
+k = 2
 
 import collections
 
-user_list = ["con", "ryan"]
-report = ["ryan con", "ryan con", "ryan con", "ryan con"]
-k = 3
+# user_list = ["con", "ryan"]
+# report = ["ryan con", "ryan con", "ryan con", "ryan con"]
+# k = 3
+
 
 def solution(id_list, report, k):
     answer = []
@@ -42,3 +44,27 @@ def solution(id_list, report, k):
 
 
 print(solution(user_list, report, k))
+
+'''
+다른사람 베스트 풀이
+'''
+
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)  # id_list 만큼 길이를 만든다.
+    reports = {x: 0 for x in id_list}  # id_list 로 미리 보고된 object 를 만든다.
+
+    for r in set(report):  # set로 중복제거
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
+
+print(solution(user_list, report, k))
+
+'''
+다른사람의 코드를 봤는데... 너무 깔끔했다....할말을 잃음
+'''
